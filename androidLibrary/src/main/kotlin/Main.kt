3acc88ef1d1eb24e57372b6c3adfafd54ecc7cc0
AssasinNik <nikitacherenkov228@gmail.com>
2024-10-29 19:59:28 +0300

    Synhronise Coroutines(Actor(Channels))

--- androidLibrary/src/main/kotlin/Main.kt
+++ androidLibrary/src/main/kotlin/Main.kt
@@ -1,99 +1,101 @@
 package org.example
 
-import com.sun.source.tree.Scope
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
-import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-import javax.xml.crypto.Data
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.channels.actor
 
-class E {
+
+fun main(){
 
 }
 
-suspend fun main(){
-    //Каналы(Channels)
-    //Основная цель коммуникация между несколькими корутинами
-    //send и receive(suspend функции) основные методы для получения и отправки значения из канала и в канал
-    //получить все значения из канала можно и просто проитерировав его
-    val channel: Channel<E> = Channel()
-    for(value in channel){
-        //обрабаотка данных
+//Синхронизация между корутинами
+//Пример когда 2 корутины могут обращаться к одному ресурсу
+suspend fun doWork(){
+    var counter = 0
+    coroutineScope {
+        launch(Dispatchers.Default) {
+            counter+=10
+        }
+        launch(Dispatchers.Default) {
+            counter+=10
+        }
     }
-    //trySend и tryRecieve не suspend функцяи
-    //может быть вызвана не из корутины
-    //что позволяет отправить и получить значение, если канал имеет место в буффер, или у него уже есть значение в буффере
+}
 
-    //Закрытие канала
-    channel.close()
+//Принципы синхронизации
+//Только один поток может иметь доступ в критическую секцию в определнный момент времени
+//Либо за счет ограничения доступа, либо за счет обеспечения очередности
 
-    //проверить канал на то открыт ли он на отправку или получение модно с помощью
-    channel.isClosedForSend
-    channel.isClosedForReceive
-    //Одновременно у канала может быть больше одного получателя так и больше одного отправщика
-    //Каналы расчитаны на работу с несколькими корутинами
+//Снятие блокировки должно происходить на том же потоке, на котором она была захвачена
+//Иначе ресурс зависнет навсегда
 
-    //Channel может приводить к приостановке корутины, пока он не сможет обработать значение(на отправку/получение)
-    //поэтому не стоит забывать закрывтаь channel
+//Так как suspend функции могут происходить на разных потоках
+//Следовательно между захватом и снятие блокировки не должно быть вызовов suspend функций
 
-    //По умолчанию у канал нет буфера, следовательно send будет приостановлен
-    //При создании канала можно указать capacity
-    //Тогда операцию send можно будет даже отправлять, если еще нету никаких получателей, а значение поместится в буффер и будет отправлено позже
-    val receiveChannel: Channel<E> = Channel()
+//Критическая секция-участок исполняемого кода программы, в котором производится доступ к общему ресурсу
+//не должен быть одновременно использован более чем одним потоком
 
-    //Размеры буфера Channel
-    //Rendezvous-без буфера(по умолчанию)
-    //Conflated-размер буффера 1(хранит только последнее полученное значение)
-    //Buffered-стандартный размер буфера определен в свойствах окружения(по умолчанию 64)
-    //Максимально возможный размер буффера(Int.MAX_SIZE)
-    //Можно в целом указать свою capacity(цифру) главное чтобы оно не перекрывала какую либо из константных
-    val channel1: Channel<E> = Channel(capacity = BUFFERED)
 
-    //При создании канала можно указать политику поведения send
-    //suspend-политика по умолчанию
-    //Корутина будет приостанволена, если значение принять сейчас некому, а буфер переполнен или отсутствует
-
-    //DROP_OLDEST(очередь), DROP_LATEST(стек)
-    //Удаляет значение(самые старые/самые новые) в буфере при его переполнении. Вызов send никогда не приостанвоит корутину,
-    //а trySend() всегда успешно выполнится при наличии буффера
-
-    //Горячий Flow
-    //Значения могут быть получены лишь один раз
-    // при повторном вызове collect у Flow будет краш
-    receiveChannel.consumeAsFlow()
-
-    //Горячий Flow
-    //Может использоваться для нескольких получателей значений
-    //множественный вызов collect
-    receiveChannel.receiveAsFlow()
-
-    //Чаще всего не придется создавать самому channel(Чаще всего встретим билдеры produce(receiveChannel) и actor(SendChannel))
-    //produce-служит для генерации конечных или бесконечных потоков значений
-    val scope =CoroutineScope(CoroutineName("coroutine1")+Dispatchers.IO+ SupervisorJob())
-    val channel2 : ReceiveChannel<Int> =
-        scope.produce {
-            var x = 1
-            while (true){
-                send(x++)
+//Как обеспечить последовательность
+//Один из вариантов создание своего dispatcher который будет работать на одном выделенном потоке
+@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+suspend fun doSome(){
+    val counterContext = newSingleThreadContext("Counter")
+    var counter = 0
+    val scope = CoroutineScope(CoroutineName("Scope 1"))
+    val jobs = List(100){
+        scope.launch(start = CoroutineStart.LAZY) {
+            repeat(1_000){
+                withContext(counterContext){
+                    counter+=10
+                }
             }
         }
-    scope.launch {
-        val data = channel.receive()
     }
-    //actor служит для получения значения извне(помогате синхронизировать работу некольких корутин)
-    val actor: SendChannel<Any> = scope.actor {
-        val data = receive()
-    }
-    scope.launch {
-        actor.send("")
-    }
-
-    
-
-
+    jobs.joinAll()
+    counterContext.close()//обязательно закрыть CoroutineContext после того как он не нужен
 }
 
+//Channel для синхронизации
+//Синхронизация через коммуникацию
 
+//Вариант с actor
+// Определяем различные типы сообщений для работы с общим ресурсом
+sealed class CounterMsg{
+    class Increment: CounterMsg()
+    class GetValue(val response: CompletableDeferred<Int>): CounterMsg()
+}
+// Функция для создания актора, управляющего доступом к счетчику
+@OptIn(ObsoleteCoroutinesApi::class)
+fun CoroutineScope.counterActor() = actor<CounterMsg>(capacity = BUFFERED) {
+    var counter = 0 // общий ресурс, защищенный актером
+    for (msg in channel) { // обрабатываем каждое сообщение по очереди
+        when (msg) {
+            is CounterMsg.Increment -> counter++
+            is CounterMsg.GetValue -> msg.response.complete(counter)
+        }
+    }
+}
+fun someWork() = runBlocking {
+    // Создаем актер для управления счетчиком
+    val counter = counterActor()
 
+    // Запускаем 1000 корутин, каждая из которых увеличивает значение счетчика
+    val jobs = List(1000) {
+        launch {
+            counter.send(CounterMsg.Increment())
+        }
+    }
+
+    // Ждем завершения всех корутин
+    jobs.forEach { it.join() }
+
+    // Получаем текущее значение счетчика
+    val response = CompletableDeferred<Int>()
+    counter.send(CounterMsg.GetValue(response))
+    println("Final counter value: ${response.await()}")
+
+    // Закрываем актор
+    counter.close()
+}
