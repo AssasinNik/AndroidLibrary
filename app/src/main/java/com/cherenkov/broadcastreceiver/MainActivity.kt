8a4ca5682e7a920e0490ad138d9d5cc09c64cb39
AssasinNik <nikitacherenkov228@gmail.com>
2025-01-24 13:07:59 +0300

    Dynamic Broadcast Receiver

---
+++ app/src/main/java/com/cherenkov/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.cherenkov.broadcastreceiver
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangedReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        receiver = AirplaneModeChangedReceiver()
+
+        //IntentFilter используется чтобы система поределяла, какие приложения хотят получать какие intent
+        //Здесь мы сообщаем что хотим получать intents об изменениях авиарежима
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                registerReceiver(receiver, it, Context.RECEIVER_NOT_EXPORTED)
+            } else {
+                // Для старых версий
+                registerReceiver(receiver, it)
+            }
+        }
+
+        //Существуют два типа broadcast receiver: статические и динамические
+        //Статические обновляются в манифесте и работают даже если ваше приложение закрыто
+        //Динамические работают если приложение работает или свернуто
+
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+}
